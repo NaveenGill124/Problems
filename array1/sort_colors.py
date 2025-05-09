82c5b7ad530142a144794c5df8a7af48c859a3d2
Naveen Gill <55091000+NaveenGill124@users.noreply.github.com>
2025-05-10 00:00:28 +0530

    Update sort_colors.py

--- array1/sort_colors.py
+++ array1/sort_colors.py
@@ -3,23 +3,50 @@ class Solution:
         
         nums.sort()
 # ----------------------------- build in function which takes O(nlogn) time complexity --------------------------
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+                
+        x, y, z = 0, 0, 0
 
+        for num in nums:
+            
+            if num == 0:
+                x = x + 1
+            elif num == 1:
+                y = y + 1
+            elif num == 2:
+                z = z + 1
+            
+        for i in range(0, x):
+            nums[i] = 0
+
+        for i in range(x, x+y):
+            nums[i] = 1
+        
+        for i in range(x+y, x+y+z):
+            nums[i] = 2
+
+# ----------------------------- slightly better approach --------------------------
 
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         
-        low, mid, high = 0, 0, len(nums) - 1
-        
-        while mid <= high:
+        n = len(nums)
+        mid,  low = 0, 0
+        high = n-1
+        while (mid <= high):
+
             if nums[mid] == 0:
-                nums[low], nums[mid] = nums[mid], nums[low]
-                low += 1
-                mid += 1
+                nums[mid], nums[low] = nums[low], nums[mid]
+                low = low + 1
+                mid = mid + 1
+            
             elif nums[mid] == 1:
-                mid += 1
-            else:  # nums[mid] == 2
+                mid = mid + 1
+            
+            elif nums[mid] == 2:
                 nums[mid], nums[high] = nums[high], nums[mid]
-                high -= 1
+                high = high - 1
 
               
 # Time Complexity: O(n) 
