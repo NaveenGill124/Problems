44659df7d1628be2cdda0613de3b2fadc1bd72ca
Naveen Gill <55091000+NaveenGill124@users.noreply.github.com>
2025-06-22 02:25:19 +0530

    Update Subsets.py

--- Recursion/Subsets.py
+++ Recursion/Subsets.py
@@ -1,1 +1,18 @@
-
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+        result = []
+        
+        def backtrack(start, path):
+            # path is the current subset I have built so far.
+            # Append a copy of path to the results list.
+            result.append(path[:])  
+            
+            # Now, adding each of the remaining elements one by one.
+            for i in range(start, len(nums)):
+                path.append(nums[i])       # Choose this element
+                backtrack(i + 1, path)     # Recurse with next elements
+                path.pop()                 # Backtrack: remove the element  
+        
+        backtrack(0, [])
+        return result
