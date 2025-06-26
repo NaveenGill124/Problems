ea104ca049d67514cc1b416328f7ea703a69654c
Naveen Gill <55091000+NaveenGill124@users.noreply.github.com>
2025-06-26 12:07:37 +0530

    Update Combination_sum-2.py

--- Recursion/Combination_sum-2.py
+++ Recursion/Combination_sum-2.py
@@ -1,1 +1,33 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
 
+        candidates.sort()
+        n = len(candidates)
+
+        result = []   
+        comb = []       
+
+        def comSum(idx, t, arr):
+
+            if t == 0:
+                result.append(arr[:])
+                return
+            
+            if t < 0 or idx == n:
+                return
+            
+            for i in range(idx, n):
+
+                if i> idx and candidates[i] == candidates[i-1]:
+                    continue
+
+                arr.append(candidates[i])
+
+                comSum(i+1, t-candidates[i], arr)
+
+                arr.pop()
+        
+        comSum(0, target, [])
+
+        
+        return result
