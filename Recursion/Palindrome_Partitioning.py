34451d5817943274467e9b2db0b7de61c64c142a
Naveen Gill <55091000+NaveenGill124@users.noreply.github.com>
2025-06-26 13:35:15 +0530

    Update Palindrome_Partitioning.py

--- Recursion/Palindrome_Partitioning.py
+++ Recursion/Palindrome_Partitioning.py
@@ -1,1 +1,25 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
 
+
+        result = []
+        arr = []
+
+        def palindrome(sub):
+            return sub == sub[::-1]
+
+        def backtrack(idx: int):
+
+            if idx == len(s):
+                result.append(arr[:])
+                return
+
+            for i in range(idx + 1, len(s) + 1):
+                substring = s[idx:i]
+                if palindrome(substring):
+                    arr.append(substring)
+                    backtrack(i)
+                    arr.pop()
+
+        backtrack(0)
+        return result
