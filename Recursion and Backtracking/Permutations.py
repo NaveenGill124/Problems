fa651bb51064c81a35fbdd16857b2177a118c5e4
Naveen Gill <55091000+NaveenGill124@users.noreply.github.com>
2025-06-27 16:20:24 +0530

    Update Permutations.py

--- Recursion and Backtracking/Permutations.py
+++ Recursion and Backtracking/Permutations.py
@@ -1,1 +1,23 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        result = []
+        n = len(nums)
+        def revert(nums, idx, result):
 
+            if idx == n:
+
+                result.append(nums[:])
+                return
+
+            for i in range(idx, n):
+
+                nums[idx], nums[i] = nums[i], nums[idx]
+
+                revert(nums, idx+1, result)
+
+                nums[idx], nums[i] = nums[i], nums[idx]
+
+        revert(nums, 0, result)
+
+        return result
