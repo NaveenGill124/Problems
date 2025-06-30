497193558f0abcf080ada56d4895db23d743f41f
Naveen Gill <55091000+NaveenGill124@users.noreply.github.com>
2025-06-30 18:34:11 +0530

    Update Search_in_Rotated_Sorted_Array.py

--- Binary Search/Search_in_Rotated_Sorted_Array.py
+++ Binary Search/Search_in_Rotated_Sorted_Array.py
@@ -14,15 +14,17 @@ class Solution:
             
             if nums[low] <= nums[mid]:
 
-                if nums[low] <= target <= nums[mid-1]:
+                if nums[low] <= target < nums[mid]:
                     high = mid - 1
                 else:
                     low = mid + 1
             
             else:
-                if nums[mid+1] <= target <= high:
+                if nums[mid] < target <= nums[high]:
                     low = mid + 1
                 else:
                     high = mid -1
         
         return -1
+
+        
